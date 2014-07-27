6179ee24c03181fb2f4a7da5954f8613884ef137
EugeneDeRastignac <tomas.simovic@gmail.com>
2014-07-27 22:42:15 +0200

    new commit
    commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions that allow to speed up
+## the computation of the inverse of a matrix through the
+## use of cache 
+## Example usage:
+## c=rbind(c(1, -1/4), c(-1/4, 1)) 
+##> m <- makeCacheMatrix(c)
+##> cacheSolve(m)
+##          [,1]      [,2]
+##  [1,] 1.0666667 0.2666667
+##  [2,] 0.2666667 1.0666667
+## > cacheSolve(m)
+## getting cached data
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
 
-## Write a short comment describing this function
+
+## creates a special matrix object containing a list of functions
+## to get and set the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	  inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+
+## Returns a matrix that is the inverse of 'x' using the cache 
+## (inverse is not computed if the result is already stored
+## in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
+
+
 }
